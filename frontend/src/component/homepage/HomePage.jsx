c8440a0da1659b639638994d00550348a4656535
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-09-14 23:42:07 +0100

    Added homepage css file

---
+++ frontend/src/component/homepage/HomePage.jsx
@@ -0,0 +1,13 @@
+import image from "../homepage/assets/home.png"
+
+const HomePage = () =>{
+    return(
+        <>
+        <div className="first-step">
+            <img src={image} alt="Riders" />
+        </div>
+        </>
+    );
+}
+
+export default HomePage;
