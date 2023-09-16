7bbed63b82178174d5e4f341c5410da1ccf60cd5
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-09-16 21:58:45 +0100

    Modified NavBar component to be dynamic

--- frontend/src/component/homepage/HomePage.jsx
+++ frontend/src/component/homepage/HomePage.jsx
@@ -1,13 +1,21 @@
 import image from "../homepage/assets/home.png";
 import "../homepage/HomePage.css";
-const HomePage = () =>{
-    return(
-        <>
+const HomePage = () => {
+  return (
+    <>
+      <div className="container">
         <div className="first-step">
-            <img src={image} alt="Riders" />
+          <img src={image} alt="Riders" />
         </div>
-        </>
-    );
-}
+
+        <div className="second-step">
+            <h3>What make us unique?</h3>
+            <p>Go2Geda is an innovative mobility solution that empowers users to efficiently book transportation and share costs<br /> with others who share similar travel patterns. It not only optimizes transportation logistics but also fosters social <br />network connectivity, enhancing the overall commuting experience.</p>
+
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
