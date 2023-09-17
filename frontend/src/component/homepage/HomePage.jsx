f62ec98bd5b1947084c1fc152728f596c17880d7
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-09-17 02:19:28 +0100

    Working on home page third step

--- frontend/src/component/homepage/HomePage.jsx
+++ frontend/src/component/homepage/HomePage.jsx
@@ -1,5 +1,9 @@
 import image from "../homepage/assets/home.png";
 import "../homepage/HomePage.css";
+import { Icon } from "@iconify/react";
+import userFriends from "@iconify/icons-fa-solid/user-friends";
+import currencyNaira from '@iconify/icons-tabler/currency-naira';
+
 const HomePage = () => {
   return (
     <>
@@ -9,11 +13,35 @@ const HomePage = () => {
         </div>
 
         <div className="second-step">
-            <h3>What make us unique?</h3>
-            <p>Go2Geda is an innovative mobility solution that empowers users to efficiently book transportation and share costs<br /> with others who share similar travel patterns. It not only optimizes transportation logistics but also fosters social <br />network connectivity, enhancing the overall commuting experience.</p>
+          <h3>What make us unique?</h3>
+          <p>
+            Go2Geda is an innovative mobility solution that empowers users to
+            efficiently book transportation and share costs
+            <br /> with others who share similar travel patterns. It not only
+            optimizes transportation logistics but also fosters social <br />
+            network connectivity, enhancing the overall commuting experience.
+          </p>
         </div>
+
         <div className="third-step">
-          
+          <div className="info">
+            <Icon icon={userFriends} width="90" height="90" id="icon" />
+            <p>
+              Saying goodbye to expensive solo-ride and embracing <br />
+              shared ride resulting in cheaper fare s per cheap{" "}
+            </p>
+          </div>
+          <hr />
+          <div className="info">
+          <Icon icon={currencyNaira} width="90" height="90" id="icon1"/>
+          <p>
+            Enjoy you trip with minimum budget
+          </p>
+          </div>
+          <hr />
+          <div className="info">
+
+          </div>
         </div>
       </div>
     </>
